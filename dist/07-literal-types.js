38d8291287f24265151117ceefb05572963bb13f
rudra-123-patel <rudrapatel1054@gmail.com>
2026-09-22 16:26:07 +0530

    wrote details

--- dist/07-literal-types.js
+++ dist/07-literal-types.js
@@ -1,6 +1,10 @@
 "use strict";
 // string literal types
 let direction;
+direction = "north"; // Valid
+direction = "south"; // Valid
+// direction = "up"; // Error: Type '"up"' is not assignable to type '"north" | "south" | "east" | "west"'.
 // numeric literal types
-// combined literal types
+let diceRoll;
+diceRoll = 3; // Valid
 //# sourceMappingURL=07-literal-types.js.map
--- src/07-literal-types.ts
+++ src/07-literal-types.ts
@@ -1,7 +1,26 @@
 // string literal types
 
 let direction: "north" | "south" | "east" | "west";
+direction = "north"; // Valid
+direction = "south"; // Valid
+// direction = "up"; // Error: Type '"up"' is not assignable to type '"north" | "south" | "east" | "west"'.
 
 // numeric literal types
 
+let diceRoll: 1 | 2 | 3 | 4 | 5 | 6;
+diceRoll = 3; // Valid
+// diceRoll = 7; // Error: Type '7' is not assignable to type '1 | 2 | 3 | 4 | 5 | 6'.
+
 // combined literal types
+
+type SucessResponse = {
+  status: "success";
+  data: any;
+};
+
+type ErrorResponse = {
+  status: "error";
+  message: string;
+};
+
+type ApiResponse = SucessResponse | ErrorResponse;
