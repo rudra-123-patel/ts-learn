b234f2a5f1636222de4e41cc173d50bc588c7d25
rudra-123-patel <rudrapatel1054@gmail.com>
2026-09-24 15:24:12 +0530

    more in 09-classs

--- src/09-classes.ts
+++ src/09-classes.ts
@@ -1,1 +1,14 @@
-class Person {}
+class Person {
+  // properties
+  private name: string;
+  protected age: number;
+  public email: string;
+
+  // constructor
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+}
